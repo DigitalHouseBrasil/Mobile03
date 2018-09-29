595ea0d0c1f12f1f807f3043bf997671a1041cdf
samirassaf <samir_assaf_1@hotmail.com>
2018-09-29 16:38:21 -0300

    arquivo

--- Android/DigitalHouseApp/app/src/main/java/br/com/digitalhouse/app/adapters/RecyclerViewPersonAdapter.java
+++ Android/DigitalHouseApp/app/src/main/java/br/com/digitalhouse/app/adapters/RecyclerViewPersonAdapter.java
@@ -9,16 +9,20 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import br.com.digitalhouse.app.R;
+import br.com.digitalhouse.app.interfaces.RecyclerViewOnItemClickListener;
 import br.com.digitalhouse.app.model.Person;
 
 import java.util.List;
 
 public class RecyclerViewPersonAdapter extends RecyclerView.Adapter<RecyclerViewPersonAdapter.ViewHolder> {
-
+    //Lista de pessoas
     private List<Person> personList;
+    //Evento de click
+    private RecyclerViewOnItemClickListener listener;
 
-    public RecyclerViewPersonAdapter(List<Person> personList) {
+    public RecyclerViewPersonAdapter(List<Person> personList, RecyclerViewOnItemClickListener listener) {
         this.personList = personList;
+        this.listener = listener;
     }
 
     @NonNull
@@ -31,19 +35,28 @@ public class RecyclerViewPersonAdapter extends RecyclerView.Adapter<RecyclerView
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // Pego a pessoa na posição enviada
-        Person person = personList.get(position);
+       final Person person = personList.get(position);
 
         //Preenche os dados da pessoa nas views
         holder.bind(person);
 
         //Seta a animação para cada item
         setAnimation(holder.itemView);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Veiculamos o click no item
+                listener.onItemClick(person);
+
+            }
+        });
+
     }
 
     // Set a animação em uma view
     private void setAnimation(View view) {
-        Animation animation = AnimationUtils.loadAnimation(view.getContext(), R.anim.bounce);
-        animation.setDuration(1000);
+        Animation animation = AnimationUtils.loadAnimation(view.getContext(), R.anim.slide_in_left);
         view.startAnimation(animation);
     }
 
---
+++ Android/DigitalHouseApp/app/src/main/java/br/com/digitalhouse/app/interfaces/RecyclerViewOnItemClickListener.java
@@ -0,0 +1,8 @@
+package br.com.digitalhouse.app.interfaces;
+
+import br.com.digitalhouse.app.model.Person;
+
+public interface RecyclerViewOnItemClickListener {
+
+    void onItemClick (Person person);
+}

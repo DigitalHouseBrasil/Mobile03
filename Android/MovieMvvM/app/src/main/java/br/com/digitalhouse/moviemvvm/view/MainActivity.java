6651a86791e937f10fc4e4160409c47d1401ebad
tairoroberto <tairoroberto@hotmail.com>
2018-11-06 22:28:19 -0200

    Add pagination

--- Android/MovieMvvM/app/src/main/java/br/com/digitalhouse/moviemvvm/view/MainActivity.java
+++ Android/MovieMvvM/app/src/main/java/br/com/digitalhouse/moviemvvm/view/MainActivity.java
@@ -1,9 +1,15 @@
 package br.com.digitalhouse.moviemvvm.view;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Declara os componentes que serão usados
     private RecyclerView recyclerViewMovies;
+    private ProgressBar progressBar;
     private RecyclerViewMovieAdapter adapter;
     private List<Movie> movies = new ArrayList<>();
     private MovieViewModel viewModel;
@@ -29,17 +36,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+        recyclerViewMovies.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewMovies.setAdapter(adapter);
+        viewModel.getMovies("top_rated", "pt-BR", pag, "BR");
+        viewModel.moviesLiveData.observe(this, movies -> adapter.setMovies(movies));
+        viewModel.isLoading.observe(this, loading -> {
+            if (loading){
+                progressBar.setVisibility(View.VISIBLE);
+            }else {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
 
-        // TODO setar LayoutManager no recyclerview
-        // TODO setar adapter no recyclerview
-        // TODO chamar api para buscar os filmes
-        // TODO implementar paginação
-        // TODO add observer para o liveData movies do viewmodel
+        recyclerViewMovies.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                int totalItemCount = layoutManager.getItemCount();
+                int lastVisible = layoutManager.findLastVisibleItemPosition();
+
+                boolean endHasBeenReached = lastVisible + 5 >= totalItemCount;
+
+                if (totalItemCount > 0 && endHasBeenReached && pag <= totalPages) {
+                    pag++;
+                    viewModel.getMovies("top_rated", "pt-BR", pag, "BR");
+                }
+            }
+        });
     }
 
     // Inicializa as Views
     private void initViews() {
         recyclerViewMovies = findViewById(R.id.recyclerview_movies);
+        progressBar = findViewById(R.id.progress_bar);
         adapter = new RecyclerViewMovieAdapter(movies);
         viewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
     }
--- Android/MovieMvvM/app/src/main/java/br/com/digitalhouse/moviemvvm/viewmodel/MovieViewModel.java
+++ Android/MovieMvvM/app/src/main/java/br/com/digitalhouse/moviemvvm/viewmodel/MovieViewModel.java
@@ -3,28 +3,89 @@ package br.com.digitalhouse.moviemvvm.viewmodel;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.MutableLiveData;
-import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import java.util.List;
 
 import br.com.digitalhouse.moviemvvm.model.Movie;
+import br.com.digitalhouse.moviemvvm.model.MovieResponse;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+import static br.com.digitalhouse.moviemvvm.data.database.MovieDatabase.getDatabase;
+import static br.com.digitalhouse.moviemvvm.data.network.RetrofitService.API_KEY;
+import static br.com.digitalhouse.moviemvvm.data.network.RetrofitService.getApiService;
+import static br.com.digitalhouse.moviemvvm.util.AppUtil.isConnected;
 
 public class MovieViewModel extends AndroidViewModel {
 
     public MutableLiveData<List<Movie>> moviesLiveData = new MutableLiveData<>();
     MutableLiveData<Throwable> moviesLiveDataError = new MutableLiveData<>();
-    MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
     private CompositeDisposable disposable = new CompositeDisposable();
 
     public MovieViewModel(@NonNull Application application) {
         super(application);
     }
 
-    public void getMovies(Context context, String category, String language, int page, String country) {
-        // TODO fazer chamada da api
-        // TODO se tiver conectado buscar na api, senão pegar da base de dados "Use o método isConnected da classe AppUtil"
-        // TODO Salvar filmes buscados no banco de dados
+    public void getMovies(String category, String language, int page, String country) {
+
+        if (isConnected(getApplication())) {
+
+            disposable.add(
+                    getApiService().getMovies(category, API_KEY, language, page, country)
+                            .map(response -> saveMovies(response))
+                            .subscribeOn(Schedulers.newThread())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .doOnSubscribe(disposable -> {
+                                isLoading.setValue(true);
+                            })
+                            .doOnTerminate(() -> {
+                                isLoading.setValue(false);
+                            })
+                            .subscribe(response ->
+                                            moviesLiveData.setValue(response.getMovies())
+                                    , throwable -> {
+                                        Log.i("LOG", "Error: " + throwable.getMessage());
+                                    })
+
+            );
+
+        } else {
+
+            disposable.add(
+                    getDatabase(getApplication()).movieDAO().getAll()
+                            .subscribeOn(Schedulers.newThread())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .doOnSubscribe(disposable -> {
+                                isLoading.setValue(true);
+                            })
+                            .doOnTerminate(() -> {
+                                isLoading.setValue(false);
+                            })
+                            .subscribe(movies ->
+                                            moviesLiveData.setValue(movies)
+                                    , throwable -> {
+                                        Log.i("LOG", "Error: " + throwable.getMessage());
+                                    })
+
+            );
+        }
+    }
+
+    private MovieResponse saveMovies(MovieResponse response) {
+        getDatabase(getApplication()).movieDAO().insert(response.getMovies());
+        return response;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
     }
 }
